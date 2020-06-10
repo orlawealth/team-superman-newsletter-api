ea890d7885f6940082be0b69b4e8110164841ca6
Arturo J. Delgado S <ajdelgados@gmail.com>
2020-06-10 01:42:30 -0400

    Added topics

--- SubscriptionService/app/Http/Controllers/SubscriberController.php
+++ SubscriptionService/app/Http/Controllers/SubscriberController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Subscriber;
+use App\Topic;
+use App\TopicSubscriber;
 
 class SubscriberController extends Controller
 {
-    public function index(Request $request)
-    {   
+    public function index(Request $request, $topicRequest)
+    {
         $this->validate($request, [
             'email' => 'required|email',
         ]);
@@ -17,13 +19,30 @@ class SubscriberController extends Controller
         if(!$subscriber) {
             $subscriber = new Subscriber();
             $subscriber->Email = $request->email;
+            $subscriber->Status = 1;
+            $subscriber->save();
         }
-        $subscriber->Status = 1;
-        $subscriber->save();
-        return response()->json($subscriber);
+
+        $topic = Topic::where('Name', '=', $topicRequest)->first();
+        if(!$topic) {
+            $topic = new Topic();
+            $topic->Name = $topicRequest;
+            $topic->save();
+        }
+
+        $topicSubscriber = TopicSubscriber::where('Topic', $topic->id)->where('Subscriber', $subscriber->id)->first();
+        if(!$topicSubscriber) {
+            $topicSubscriber = new TopicSubscriber();
+            $topicSubscriber->Topic = $topic->id;
+            $topicSubscriber->Subscriber = $subscriber->id;
+        }
+        $topicSubscriber->Status = 1;
+        $topicSubscriber->save();
+
+        return response()->json(['id'=> $topicSubscriber->id, 'Email'=> $subscriber->Email, 'Status' => $topicSubscriber->Status]);
     }
 
-    public function unsubscribe(Request $request)
+    public function unsubscribe(Request $request, $topicRequest)
     {
         $this->validate($request, [
             'email' => 'required|email',
@@ -31,18 +50,49 @@ class SubscriberController extends Controller
         
         $subscriber = Subscriber::where('Email', '=', $request->email)->first();
         if($subscriber) {
-            $subscriber->Status = 0;
-            $subscriber->save();
-            return response()->json($subscriber);
-        }  
+
+            $topic = Topic::where('Name', '=', $topicRequest)->first();
+            if(!$topic) {
+                $topic = new Topic();
+                $topic->Name = $topicRequest;
+                $topic->save();
+            }
+
+            $topicSubscriber = TopicSubscriber::where('Topic', $topic->id)->where('Subscriber', $subscriber->id)->first();
+            if(!$topicSubscriber) {
+                $topicSubscriber = new TopicSubscriber();
+                $topicSubscriber->Topic = $topic->id;
+                $topicSubscriber->Subscriber = $subscriber->id;
+            }
+            $topicSubscriber->Status = 0;
+            $topicSubscriber->save();
+
+            return response()->json(['id'=> $topicSubscriber->id, 'Email'=> $subscriber->Email, 'Status' => $topicSubscriber->Status]);
+        }
+
         return response()->json(['error' => [
             'code' => 404,
             'message' => 'Not found',
         ]], 404);
     }
 
-    public function subscribers() {
-        $subscribers = $subscriber = Subscriber::where('Status', '=', 1)->get();
-        return response()->json($subscribers);
+    public function subscribers($topicRequest) {
+        $topic = Topic::where('Name', $topicRequest)->first();
+        if($topic) {
+            $subscribers = [];
+
+            $topicSubscribers = TopicSubscriber::where('Topic', $topic->id)->where('Status', 1)->get();
+
+            foreach($topicSubscribers as $topicSubscriber) {
+                $subscriber = Subscriber::find($topicSubscriber->Subscriber);
+                if($subscriber) {
+                    $subscribers[] = ['id' => $topicSubscriber->id, 'Email' => $subscriber->Email, 'Status' => $topicSubscriber->Status];
+                }
+            }
+
+            return response()->json($subscribers);
+        }
+
+        return response()->json([]);
     }
 }
