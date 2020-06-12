bf52f202cb6f4f0034f114c49f0350a435694547
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-06-12 17:27:45 +0100

    added ngnix

--- NewsPageService/src/views/js/layout.js
+++ NewsPageService/src/views/js/layout.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#button").click(function(){
       var subEmail = document.querySelector("#email").value;
       let subLabel=document.querySelector("#subId");
-      let endpoint ="http://localhost:11003/sendsubscription";
+      let endpoint ="http://localhost:11005/sendsubscription";
       fetch(endpoint,{
         method:"POST",
         body:JSON.stringify({email:subEmail}),
